7e0e629648db7302d8b7bec74876607e069eb962
Carl A Palumbo <carlpalumbo1@gmail.com>
2020-07-23 17:57:34 +0000

    Done.

--- src/MouseComponent.js
+++ src/MouseComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class MouseComponent extends Component {
+class MouseComponent extends Component {
 
   componentDidMount() {
     require("./hoverEffect.js")
@@ -14,3 +14,6 @@ export default class MouseComponent extends Component {
     )
   }
 }
+
+
+export default MouseComponent;
